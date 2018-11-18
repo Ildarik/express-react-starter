f5243cca0b9d1841f699596ac741788fe8da51cf
Nogmanov Ildar <nogmanov@gmail.com>
2018-11-18 13:47:29 +0300

    Fix CORS issue

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ app.use(
   })
 );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 /**
  * Listen on provided port, on all network interfaces.
  */
